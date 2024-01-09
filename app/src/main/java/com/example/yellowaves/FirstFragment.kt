ea78185f697d41950a20c0a66f3b245b0721dae9
MarieCoulay <marie.coulay@gmail.com>
2024-01-09 14:21:22 +0100

    suppression binding et bouton voir la destination

--- app/src/main/java/com/example/yellowaves/FirstFragment.kt
+++ app/src/main/java/com/example/yellowaves/FirstFragment.kt
@@ -14,13 +14,6 @@ import com.example.yellowaves.databinding.FragmentFirstBinding
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class FirstFragment : Fragment() {
-
-    private var _binding: FragmentFirstBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
-
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
@@ -29,20 +22,15 @@ class FirstFragment : Fragment() {
         )
         val verticalRecyclerView = view?.findViewById<RecyclerView>(R.id.vertical_recycler_view)
         verticalRecyclerView?.adapter = SpotAdapter()
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
         return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
-        _binding = null
     }
 }
